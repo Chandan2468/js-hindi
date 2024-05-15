3abaa6247f24f34c0a313995ea3ea24608e939de
Chandan2468 <154402129+Chandan2468@users.noreply.github.com>
2024-05-15 06:38:15 +0000

    Variable Disussion

---
+++ 01_Basics/01_Variable.js
@@ -0,0 +1,13 @@
+const accountId = 154454
+let accountEmail = "chandan@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 245454 ctrl + /
+accountEmail = "malay@google.com"
+accountPassword = "1234"
+accountCity = "Mumbai"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
