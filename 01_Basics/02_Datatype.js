a7d669e9f0c26a4f211e196c568905977499202f
Chandan2468 <154402129+Chandan2468@users.noreply.github.com>
2024-05-15 06:39:47 +0000

    Datatypes

---
+++ 01_Basics/02_Datatype.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all js code as newer version 
+
+
+// alert(hello)
+console.log(3+3)
+
+let age = 18
+console.log("hitesh")
+
+// null - standalone value i.e. representation of empty value and it's type is an object
+// undefined - value is not assigned
+// symbol - uniqueness
+// boolean 
+// number
+// bigint
+// object
+
+console.log(typeof "hitesh")
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof string)
+console.log(typeof undefined)
