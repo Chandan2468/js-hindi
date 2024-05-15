807b256d18be5040785122ec9b01d143aa81b87b
Chandan2468 <154402129+Chandan2468@users.noreply.github.com>
2024-05-15 06:54:44 +0000

    Comparision operator

---
+++ 01_Basics/04_Comparision.js
@@ -0,0 +1,18 @@
+console.log(2 > 1);
+console.log(2 == 1);
+console.log("--------");
+console.log("2" > 1);       // try to compare same data type here js will convert "2" to 2
+console.log("02" > 1);
+
+console.log("--------");
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);     // value conversion problem
+
+console.log("--------");
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined >= 0);
+
+
+// === this checks data type as well as value
